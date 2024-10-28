b38657f7be4a27d278861862b747fbab0831057b
AC244 <ac03basketball24@gmail.com>
2024-10-28 08:53:01 -0500

    sixth problem

--- exercises.js
+++ exercises.js
@@ -1,6 +1,6 @@
 // Use `.map()` to iterate over the following array:
 // 1
-const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+// const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 
 // Create a new array where each value is multiplied by 2 and log the new array.
 
@@ -23,10 +23,10 @@ const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 
 // Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
 // 3
-const car = {
-    make: 'Audi',
-    model: 'q5',
-  }
+// const car = {
+//     make: 'Audi',
+//     model: 'q5',
+//   }
   
 //   // Your code here
 // const { make, model } = car
@@ -36,9 +36,37 @@ const car = {
 
 // Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
 // 4
-const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
 
+// // Your code here
+// const controversialPizzaToppings = [...pizzaToppings]
+
+// console.log(controversialPizzaToppings)
+
+// Duplicate the following object and spread its values into a new variable `myCar`.
+// 5
+// const car = {
+//     make: 'Audi',
+//     model: 'q5',
+//   };
+  
+//   // Change the `model` property of `myCar` to 'q7'. Log both objects.
+  
+//   // Your code here
+// const myCar = {...car, model: 'q7'} 
+
+// console.log(car)
+// console.log(myCar)
+
+// Create an object named userProfile. 
+// Define a variable named propertyName and assign a string to it (like a username, age, or email). 
+// Use propertyName as a dynamic key in userProfile, assigning a relevant value.
+// 6
 // Your code here
-const controversialPizzaToppings = [...pizzaToppings]
+const propertyName = 'username'
 
-console.log(controversialPizzaToppings)
+const userProfile = {
+  [propertyName]: 'ACarter21', 
+}
+
+console.log(userProfile)
